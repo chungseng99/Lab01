b8b1ff6154240277dcfa656dca6931cbcab3a2a5
chungseng99 <seng990417@gmail.com>
2021-03-18 14:32:59 +0800

    Exercise 8

--- Lab 1/src/WeightInMars.java
+++ Lab 1/src/WeightInMars.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * 
  * this class is to calculate weight of a person in mars
@@ -9,19 +12,19 @@ public class WeightInMars {
 	 * Main entry point of program
 	 */
 	public static void main(String[] args) {
+		List<Person> person = new ArrayList<Person>();
 		
-		Person person1= new Person("Chris",65.6);
-		Person person2= new Person("Mary",50.2);
-		Person person3= new Person("Mark",78.3);
+		//object declaration
+		person.add(new Person("Chris",65.6));
+		person.add(new Person("Mary",50.2));
+		person.add(new Person("Mark",78.3));
 		
 		
-		
-		System.out.println("Name:"  + person1.getName() + "\nWeight in Earth : " + person1.getWeightInEarth()+" kg" + 
-				"\nWeight in Mars: "+ person1.WeightCalculator()+ " kg");
-		System.out.println("\nName:"  + person2.getName() + "\nWeight in Earth : " + person2.getWeightInEarth()+" kg" + 
-				"\nWeight in Mars: "+ person2.WeightCalculator()+ " kg");
-		System.out.println("\nName:"  + person3.getName() + "\nWeight in Earth : " + person3.getWeightInEarth()+" kg" + 
-				"\nWeight in Mars: "+ person3.WeightCalculator()+ " kg");
+		//print output
+		for(Person currentPerson:person) {
+		System.out.println("\nName:"  + currentPerson.getName() + "\nWeight in Earth : " + currentPerson.getWeightInEarth()+" kg" + 
+				"\nWeight in Mars: "+ currentPerson.WeightCalculator()+ " kg");
+		}
 
 	}
 
