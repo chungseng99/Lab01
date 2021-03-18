f5a5bee17a296d173db967c2a74446b37e460741
chungseng99 <seng990417@gmail.com>
2021-03-18 14:19:33 +0800

    Exercise 7

---
+++ Lab 1/src/Person.java
@@ -0,0 +1,54 @@
+/*
+ * this class is to create person
+ * 
+ */
+public class Person {
+
+	//attribute 
+	private String name;
+	private double weightInEarth;
+	private double weightInMars;
+	
+	//constructor
+	public Person(String name, double weightInEarth) {
+		
+		this.name=name;
+		this.weightInEarth=weightInEarth;
+		
+	}
+	
+	//setter 
+	public void setName(String name) {
+		
+		this.name=name;
+		
+	}
+	
+	//getter
+	public String getName() {
+		
+		return name;
+		
+	}
+	
+	//setter
+	public void setWeightInEarth(double weightInEarth) {
+		
+		this.weightInEarth=weightInEarth;
+		
+	}
+	
+	//getter
+	public double getWeightInEarth() {
+		
+		return weightInEarth;
+		
+	}
+	
+	public double WeightCalculator() {
+		
+		weightInMars=weightInEarth*0.38;
+		return weightInMars;
+	}
+	
+}
